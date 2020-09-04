e7a2c31b3f122a8b5e5d2dc9e95f4a049cbbb015
Elliott Hall <elliott.hall@kcl.ac.uk>
2020-09-04 16:50:29 +0100

    r5 pages updates

--- config/settings/base.py
+++ config/settings/base.py
@@ -381,7 +381,7 @@ HAYSTACK_CONNECTIONS = {
 }
 
 WAGTAIL_FRONTEND_LOGIN_TEMPLATE = 'cms/login.html'
-WAGTAILSEARCH_RESULTS_TEMPLATE = 'cms/search_results.html'
+WAGTAILSEARCH_RESULTS_TEMPLATE = 'cms/search_results_page.html'
 
 # Your stuff...
 # ------------------------------------------------------------------------------
