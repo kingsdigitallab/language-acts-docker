779aeab5ff4ad39e8a37b4e4591b8d20229eaf28
Elliott Hall <elliott.hall@kcl.ac.uk>
2020-09-10 10:10:53 +0100

    deleted cms prior to recopy

--- language_acts/cms/management/commands/wt_update_index.py
+++
@@ -1,5 +0,0 @@
-from wagtail.search.management.commands import update_index
-
-
-class Command(update_index.Command):
-    pass
--- language_acts/cms/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views.search import SearchView
-
-urlpatterns = [
-    path('search/', SearchView.as_view(), name='search'),
-]
