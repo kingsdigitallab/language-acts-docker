3adf3d3edf5f4e2304fba264913db9b97554d81e
Elliott Hall <elliott.hall@kcl.ac.uk>
2020-09-10 18:42:54 +0100

    wagtail settings update

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.urls import include, path, re_path
-# from django.conf.urls.static import static
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.views import defaults as default_views
 
@@ -36,7 +36,7 @@ urlpatterns += [
     # re_path(r'^search/', include(wagtailsearch_frontend_urls)), TODO
     re_path('', include('language_acts.cms.urls')),
     re_path('', include(wagtail_urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
