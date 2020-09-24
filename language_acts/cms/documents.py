f4b1fffd3a4997a297e9fec30eb20beb5898b168
Elliott Hall <elliott.hall@kcl.ac.uk>
2020-09-24 16:32:28 +0100

    removed recordpagedocument

--- language_acts/cms/documents.py
+++ language_acts/cms/documents.py
@@ -1,33 +1,8 @@
 from cms.models.pages import (
-    RecordPage, RecordEntry
+    RecordEntry
 )
 from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
-
-
-@registry.register_document
-class RecordPageDocument(Document):
-    class Index:
-        name = 'language_acts'
-        settings = {'number_of_shards': 1,
-                    'number_of_replicas': 0}
-
-    class Django:
-        model = RecordPage
-
-        fields = [
-            'title',
-        ]
-
-    language = fields.KeywordField(type="keyword")
-
-    def prepare_language(self, instance):
-        languages = []
-        if instance.get_languages():
-            for entry in instance.get_languages():
-                if entry.specific.language:
-                    languages.append(entry.specific.language.name)
-        return languages
 
 
 @registry.register_document
@@ -46,6 +21,10 @@ class RecordEntryDocument(Document):
 
     language = fields.KeywordField(type="keyword")
     first_letter = fields.KeywordField(type="keyword")
+    page_url = fields.TextField()
+
+    def prepare_page_url(self, instance):
+        return instance.url
 
     def prepare_first_letter(self, instance):
         return instance.title.upper()[0] if instance.title else None
@@ -53,8 +32,3 @@ class RecordEntryDocument(Document):
     def prepare_language(self, instance):
         return (instance.specific.language.name
                 if instance.specific.language else None)
-
-
-# rs = RecordSearch(None, {'language':'Italian'})
-# response = rs.execute()
-# total = response.hits.total
