8cf60147cddbded93be038ed65440ce095974c20
Elliott Hall <elliott.hall@kcl.ac.uk>
2020-09-29 15:24:32 +0100

    test coverage at 93%

--- language_acts/cms/tests/test_tags.py
+++ language_acts/cms/tests/test_tags.py
@@ -1,12 +1,15 @@
 from cms.models.pages import (
-    IndexPage
+    IndexPage, HomePage
 )
 from cms.templatetags import cms_tags
 from cms.tests.factories import (
-    IndexPageFactory, BlogIndexPageFactory, BlogPostFactory, BlogAuthorFactory
+    IndexPageFactory, BlogIndexPageFactory, BlogPostFactory,
+    BlogAuthorFactory, HomePageFactory, EventFactory, EventIndexPageFactory,
+    NewsIndexPageFactory, NewsPostFactory
 )
 from django.test import RequestFactory, TestCase
 from wagtail.core.models import Page
+from datetime import date
 
 
 class CMSTagsTestCase(TestCase):
@@ -15,7 +18,9 @@ class CMSTagsTestCase(TestCase):
     test_page_title = 'Parent Test Page'
 
     def setUp(self) -> None:
-        self.home_page, created = Page.objects.get_or_create(id=2)
+        site_root, created = Page.objects.get_or_create(id=2)
+        self.home_page = HomePageFactory.build()
+        site_root.add_child(instance=self.home_page)
         self.top_page = IndexPageFactory.build(
             title='Index Test',
             live=True
@@ -39,6 +44,141 @@ class CMSTagsTestCase(TestCase):
         self.top_page.add_child(
             instance=IndexPageFactory.build(live=True),
         )
+
+
+class TestGetSection(CMSTagsTestCase):
+
+    def test_get_section(self):
+        page = IndexPage.objects.get(title=self.test_page_title)
+        # pass bad params make sure it handles it
+        homepage = HomePage.objects.first()
+        current_section = Page.objects.ancestor_of(page,
+                                                   inclusive=True) \
+            .child_of(homepage).first()
+        self.assertTrue(cms_tags.get_section(page), current_section.pk)
+
+
+class TestBreadcrumbs(CMSTagsTestCase):
+
+    def test_breadcrumbs(self):
+        page = IndexPage.objects.get(title=self.test_page_title)
+        # pass bad params make sure it handles it
+        homepage = HomePage.objects.first()
+        context = {'request': []}
+        breadcrumbs = cms_tags.breadcrumbs(context, homepage, page)
+        self.assertIn('root', breadcrumbs)
+        self.assertIn('current_page', breadcrumbs)
+        self.assertIn('pages', breadcrumbs)
+        self.assertEqual(2, breadcrumbs['pages'].count())
+        self.assertEqual(page.pk, breadcrumbs['pages'][1].pk)
+
+
+class TestFooterMenu(CMSTagsTestCase):
+
+    def test_footer_menu(self):
+        # pass bad params make sure it handles it
+        context = {'request': []}
+        #
+        self.event_index = EventIndexPageFactory.build(
+            title='Event Index Test',
+            show_in_menus=True
+        )
+        self.home_page.add_child(
+            instance=self.event_index
+        )
+        # make sure only 4 come back
+        self.event_1 = EventFactory.build(show_in_menus=True)
+        self.event_index.add_child(instance=self.event_1)
+        self.event_index.add_child(
+            instance=EventFactory.build(show_in_menus=True))
+        self.event_index.add_child(
+            instance=EventFactory.build(show_in_menus=True))
+        footer = cms_tags.footer_menu(context, self.event_index, self.event_1)
+        self.assertIn('root', footer)
+        self.assertIn('current_page', footer)
+        self.assertIn('menu_pages', footer)
+        self.assertEqual(3, footer['menu_pages'].count())
+
+
+class TestGetHomepageEvents(CMSTagsTestCase):
+
+    def test_get_homepage_events(self) -> None:
+        self.event_2 = EventFactory.build(
+            title='Event Today',
+            date_from=date.today()
+        )
+        self.event_index = EventIndexPageFactory.build(
+            title='Event Index Test'
+        )
+        self.home_page.add_child(
+            instance=self.event_index
+        )
+        self.event_index.add_child(instance=self.event_2)
+        events = cms_tags.get_homepage_events()
+        self.assertTrue(events.count(), 1)
+        # make sure only 4 come back
+        self.event_index.add_child(instance=EventFactory.build())
+        self.event_index.add_child(instance=EventFactory.build())
+        self.event_index.add_child(instance=EventFactory.build())
+        self.event_index.add_child(instance=EventFactory.build())
+        self.event_index.add_child(instance=EventFactory.build())
+        self.event_index.add_child(instance=EventFactory.build())
+        events = cms_tags.get_homepage_events()
+        self.assertTrue(cms_tags.get_homepage_events().count(), 4)
+
+
+class TestLines(TestCase):
+
+    def test_lines(self) -> None:
+        self.assertTrue(cms_tags.lines('A\r\nB')[0] == 'A')
+        self.assertTrue(cms_tags.lines('A\nB')[0] == 'A')
+
+
+class TestQuerify(TestCase):
+
+    def test_querify(self) -> None:
+        self.assertTrue(cms_tags.querify('?q='), '?q=')
+        self.assertTrue(cms_tags.querify('test'), 'test?q=')
+
+
+class TestNewsPreview(TestCase):
+
+    def test_news_preview(self) -> None:
+        site_root, created = Page.objects.get_or_create(id=2)
+        self.home_page = HomePageFactory.build()
+        site_root.add_child(instance=self.home_page)
+        self.news_index = NewsIndexPageFactory.build()
+        self.home_page.add_child(instance=self.news_index)
+        self.news_post_1 = NewsPostFactory.build()
+        self.news_index.add_child(instance=self.news_post_1)
+        self.assertEqual(
+            cms_tags.get_news_preview()[0].pk, self.news_post_1.pk)
+        self.news_index.add_child(instance=NewsPostFactory.build())
+        self.news_index.add_child(instance=NewsPostFactory.build())
+        self.news_index.add_child(instance=NewsPostFactory.build())
+        self.news_index.add_child(instance=NewsPostFactory.build())
+        self.assertEqual(cms_tags.get_news_preview().count(), 3)
+
+
+class TestBlogPostsPreview(TestCase):
+
+    def test_get_blog_posts_preview(self) -> None:
+        site_root, created = Page.objects.get_or_create(id=2)
+        self.home_page = HomePageFactory.build()
+        site_root.add_child(instance=self.home_page)
+        blog_index = BlogIndexPageFactory.build()
+        self.home_page.add_child(instance=blog_index)
+        author_1 = BlogAuthorFactory()
+        post_1 = BlogPostFactory.build(author=author_1)
+        blog_index.add_child(instance=post_1)
+        self.assertEqual(
+            cms_tags.get_blog_posts_preview()[0].pk, post_1.pk)
+        blog_index.add_child(instance=BlogPostFactory.build(author=author_1))
+        blog_index.add_child(instance=BlogPostFactory.build(author=author_1))
+        blog_index.add_child(instance=BlogPostFactory.build(author=author_1))
+        blog_index.add_child(instance=BlogPostFactory.build(author=author_1))
+        self.assertEqual(
+            cms_tags.get_blog_posts_preview().count(), 4)
 
 
 class TestPageInSubmenu(CMSTagsTestCase):
