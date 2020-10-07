a3051413fb77c5174d02666391a24332c7373e81
Elliott Hall <elliott.hall@kcl.ac.uk>
2020-10-07 18:20:59 +0100

    white noise

--- config/settings/base.py
+++ config/settings/base.py
@@ -151,6 +151,7 @@ AUTH_PASSWORD_VALIDATORS = [
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
