f47d00f8eba0f8bfe3f04eea8608d9a8e6f31525
Elliott Hall <elliott.hall@kcl.ac.uk>
2020-10-23 14:53:42 +0100

    removed offline compress

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,14 +4,13 @@ from django_auth_ldap.config import LDAPGroupQuery, LDAPSearch, PosixGroupType
 
 from .base import *  # noqa
 from .base import env
-
+DEBUG = True
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["language_acts.info"])
-DEBUG = True
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -113,7 +112,7 @@ ANYMAIL = {
 COMPRESS_ENABLED = env.bool("COMPRESS_ENABLED", default=True)
 # https://django-compressor.readthedocs.io/en/latest/settings/#django.conf.settings.COMPRESS_URL
 COMPRESS_URL = STATIC_URL  # noqa F405
-# COMPRESS_OFFLINE = True
+COMPRESS_OFFLINE = True
 # env.bool('COMPRESS_OFFLINE', default=True)
 
 # LOGGING
