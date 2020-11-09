7ccc6d9dec7a24fca4d1cd1479e3d14fd9f8483f
Elliott Hall <elliott.hall@kcl.ac.uk>
2020-11-09 09:46:53 +0000

    before snippet

--- language_acts/static/js/bibliographic_reference.js
+++ language_acts/static/js/bibliographic_reference.js
@@ -35,17 +35,8 @@ REF_MODEL_CHOOSER_MODAL_ONLOAD_HANDLERS = {
     'choose': function(modal, jsonData) {
         console.log('setup');
         $('a.snippet-choice').on('click', function (event) {
-            /*alert('whee');
-
-            let modelMeta = {'appName': this.dataset.appName, 'modelName': this.dataset.modelName};
-            modal.respond('refChoosen', modelMeta);
-            modal.close();
-            $(".modal-backdrop").remove();*/
             let snippetData = {'href':$(this).attr('href'), 'label': $(this).html()}
             event.preventDefault();
-            window.console.log($(this).attr('href'));
-            //var pageData = $(this).data();
-            //pageData.parentId = jsonData['parent_page_id'];
             modal.respond('refChosen', snippetData);
             modal.close();
             return false;
