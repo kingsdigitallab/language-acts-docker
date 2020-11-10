01a3837447ab37dac64dd6ac33c97b18dbedefda
Elliott Hall <elliott.hall@kcl.ac.uk>
2020-11-10 15:12:02 +0000

    use default snippet chooser

--- language_acts/cms/views/ref_chooser.py
+++ language_acts/cms/views/ref_chooser.py
@@ -2,15 +2,10 @@ from typing import List, Union
 
 from django.conf import settings
 from django.contrib.admin.utils import unquote
-from django.core.paginator import Paginator
 from django.db.models import Model
 from django.shortcuts import get_object_or_404
-from django.template.response import TemplateResponse
-from django.utils.translation import gettext as _
-from wagtail.admin.forms.search import SearchForm
 from wagtail.admin.modal_workflow import render_modal_workflow
-from wagtail.search.backends import get_search_backend
-from wagtail.search.index import class_is_indexed
+from wagtail.snippets.views import chooser as snippet_chooser
 from wagtail.snippets.views.snippets import get_snippet_model_from_url_params
 
 """
@@ -35,74 +30,16 @@ def get_reference_model() -> List[Union[str, str, Model]]:
 
         return [
             app_label,
-            model_name,
+            model_name.lstrip(),
             get_snippet_model_from_url_params(app_label, model_name.lstrip())
         ]
 
 
 def choose(request):
+    """ Use the default snippet chooser function
+    but pass through the app and model from the settings"""
     app_label, model_name, model = get_reference_model()
-    if model:
-        items = model.objects.all()
-        # Preserve the snippet's model-level ordering if specified,
-        # but fall back on PK if not
-        # (to ensure pagination is consistent)
-        if not items.ordered:
-            items = items.order_by('pk')
-
-        # Search
-        is_searchable = class_is_indexed(model)
-        is_searching = False
-        search_query = None
-        if is_searchable and 'q' in request.GET:
-            search_form = SearchForm(
-                request.GET, placeholder=_(
-                    "Search %(snippet_type_name)s"
-                ) % {
-                    'snippet_type_name': model._meta.verbose_name
-                }
-            )
-
-            if search_form.is_valid():
-                search_query = search_form.cleaned_data['q']
-
-                search_backend = get_search_backend()
-                items = search_backend.search(search_query, items)
-                is_searching = True
-
-        else:
-            search_form = SearchForm(
-                placeholder=_("Search %(snippet_type_name)s") % {
-                    'snippet_type_name': model._meta.verbose_name
-                })
-
-        # Pagination
-        paginator = Paginator(items, per_page=25)
-        paginated_items = paginator.get_page(request.GET.get('p'))
-
-        # If paginating or searching, render "results.html"
-        if request.GET.get('results', None) == 'true':
-            return TemplateResponse(request,
-                                    "wagtailsnippets/chooser/results.html",
-                                    {
-                                        'model_opts': model._meta,
-                                        'items': paginated_items,
-                                        'query_string': search_query,
-                                        'is_searching': is_searching,
-                                    })
-
-        return render_modal_workflow(
-            request,
-            'wagtailsnippets/chooser/choose.html', None,
-            {
-                'model_opts': model._meta,
-                'items': paginated_items,
-                'is_searchable': is_searchable,
-                'search_form': search_form,
-                'query_string': search_query,
-                'is_searching': is_searching,
-            }, json_data={'step': 'choose'}
-        )
+    return snippet_chooser.choose(request, app_label, model_name)
 
 
 def chosen(request, pk):
