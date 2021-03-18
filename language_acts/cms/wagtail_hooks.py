f0e10ec70ffd78e6d92987671ae40c927463804c
Elliott Hall <elliott.hall@kcl.ac.uk>
2021-03-18 15:41:45 +0000

    All colours added

--- language_acts/cms/wagtail_hooks.py
+++ language_acts/cms/wagtail_hooks.py
@@ -1,4 +1,6 @@
 import wagtail.admin.rich_text.editors.draftail.features as draftail_features
+from django.templatetags.static import static
+from django.utils.html import format_html
 from draftjs_exporter.dom import DOM
 from wagtail.admin.rich_text.converters.html_to_contentstate import (
     InlineEntityElementHandler,
@@ -64,28 +66,76 @@ def bibliographic_reference_decorator(props):
     }, props['children'])
 
 
+class TextColourStyleElementHandler(InlineStyleElementHandler):
+    pass
+
+
+#     def handle_starttag(self, name, attrs, state, contentstate):
+#         # super().handle_starttag(name, attrs, state, contentstate)
+#         if state.current_block is None:
+#             # Inline style element encountered at the top level -
+#             # start a new paragraph block to contain it
+#             add_paragraph_block(state, contentstate)
+#
+#         if state.leading_whitespace == FORCE_WHITESPACE:
+#             # any pending whitespace should be output before handling this
+#             tag,
+#             # and subsequent whitespace should be collapsed into it (=
+#             stripped)
+#             state.current_block.text += ' '
+#             state.leading_whitespace = STRIP_WHITESPACE
+#
+#         inline_style_range = InlineStyleRange(self.style)
+#         inline_style_range.offset = len(state.current_block.text)
+#         state.current_block.inline_style_ranges.append(inline_style_range)
+#         state.current_inline_styles.append(inline_style_range)
+
+class TextColourStyleEntityElementHandler(InlineEntityElementHandler):
+    """
+        Database HTML to Draft.js ContentState.
+        Converts the span tag into a REF entity, with the right data.
+    """
+    mutability = 'IMMUTABLE'
+
+    def __init__(self, entity_type, text_class):
+        self.entity_type = entity_type
+        self.text_class = text_class
+
+    def get_attribute_data(self, attrs):
+        """
+        Get the reference id
+        """
+        attrs = {
+            'text_class': self.text_class
+        }
+
+        return attrs
+
+
 class TextColourDraftail:
     """
         Register Text colour inline html function, one for each colour class
     """
     feature_name = 'entry-text-colour-1'
     text_class = 'entry-colour-1'
-    type_ = 'TEXTCOLOUR'
+    text_colour_style = ''
+    type_prefix = 'TEXTCOLOUR'
     label = ''
     tag = 'textcolour'
 
-    def __init__(self, feature_name, text_class, label):
+    def __init__(self, feature_name, text_class, label, text_colour_style):
         self.feature_name = feature_name
         self.text_class = text_class
         self.label = label
-        # self.type_ = type
+        self.text_colour_style = text_colour_style
+        self.type_ = self.type_prefix + '-{}'.format(self.label)
 
     def register_text_colour_feature(self, features):
         control = {
             'type': self.type_,
             'label': self.label,
             'description': 'Text colour',
-            'style': {'color': '$bright-blue;'},
+            'style': {'color': self.text_colour_style},
         }
 
         features.register_editor_plugin(
@@ -95,7 +145,8 @@ class TextColourDraftail:
 
         db_conversion = {
             'from_database_format': {
-                'span[data-custom-style]': InlineStyleElementHandler(self.type_)},
+                'span[data-custom-style]': TextColourStyleElementHandler(
+                    self.type_)},
             'to_database_format': {'style_map': {
                 self.type_: {
                     'element': 'span',
@@ -117,24 +168,35 @@ class TextColourDraftail:
 text_colour_hooks = [
 
     TextColourDraftail(
-        'entry-text-colour-1', 'entry-colour-1', 'C1',
+        'entry-text-colour-1', 'entry-colour-1',
+        'C1', 'rgb(20, 177, 231)'
     ),
-    # TextColourDraftail(
-    #     'entry-text-colour-2', 'entry-colour-2', 'C2',
-    #     'text-colour-2'),
-    # TextColourDraftail(
-    #     'entry-text-colour-3', 'entry-colour-3', 'C3',
-    #     'text-colour-3'),
-    # TextColourDraftail(
-    #     'entry-text-colour-4', 'entry-colour-4', 'C4',
-    #     'text-colour-4'),
-    # TextColourDraftail(
-    #     'entry-text-colour-5', 'entry-colour-5', 'C5',
-    #     'text-colour-5'),
+    TextColourDraftail(
+        'entry-text-colour-2', 'entry-colour-2',
+        'C2',
+        'rgb(186, 75, 22)'),
+    TextColourDraftail(
+        'entry-text-colour-3', 'entry-colour-3', 'C3',
+        'rgb(0, 45, 59)'),
+    TextColourDraftail(
+        'entry-text-colour-4', 'entry-colour-4', 'C4',
+        '#3ADB76'),
+    TextColourDraftail(
+        'entry-text-colour-5', 'entry-colour-5', 'C5',
+        '#EC5840'),
 ]
 
 for hook in text_colour_hooks:
     hooks.register(
         'register_rich_text_features',
         hook.register_text_colour_feature
+    )
+
+
+@hooks.register('insert_editor_css')
+def editor_css():
+    """ Add css for text colours and other custom styles"""
+    return format_html(
+        '<link rel="stylesheet" href="{}">',
+        static('css/wagtail/editor.css')
     )
