21069e3077c4ec9b3fca13a76e996c2729859040
Elliott Hall <elliott.hall@kcl.ac.uk>
2021-05-06 18:31:18 +0100

    glossary tag bug

--- language_acts/cms/templatetags/cms_tags.py
+++ language_acts/cms/templatetags/cms_tags.py
@@ -351,7 +351,7 @@ def get_prefix_suffix(result):
 def add_glossary_terms(value: str) -> str:
     # <span data-term_id="1">[term_1_second-sentence]</span>
     ref_path = re.compile(
-        r'<span data-term_id="(\d+)">(.*?)\[term_\d+_(.*?)\](.*?)</span>('
+        r'<span data-term_id="(\d+)">(.*?)\[term_\d+_*(.*?)\](.*?)</span>('
         r'</span>)*'
     )
     if type(value) == RichText:
