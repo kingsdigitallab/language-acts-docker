599e54497a777be88e2c371492d424c4f15c7d48
Elliott Hall <elliott.hall@kcl.ac.uk>
2023-06-15 14:15:06 +0100

    root

--- language_acts/cms/templatetags/cms_tags.py
+++ language_acts/cms/templatetags/cms_tags.py
@@ -175,26 +175,26 @@ def main_menu(context, root, current_page=None):
         request = context["request"]
     else:
         request = None
-    # if request is not None and root is None:
-    #     root = Site.find_for_request(context["request"]).root_page
-    # if root is None:
-    #     root = current_page
-    # try:
-    #     menu_pages = root.get_children().live().in_menu()
-    #     root.active = current_page.url == root.url if current_page else False
-    #     if menu_pages:
-    #         try:
-    #             for page in menu_pages:
-    #                 page.active = (
-    #                     current_page.url.startswith(
-    #                         page.url) if current_page else False
-    #                 )
-    #         except ValueError:
-    #             print("Valure error for {}".format(page))
-    # except AttributeError:
-    #     print("Error in root: {}:{}".format(root, current_page))
-    #     menu_pages = []
-    menu_pages = []
+    if request is not None and root is None:
+        root = Site.find_for_request(context["request"]).root_page
+    if root is None:
+        root = current_page
+    try:
+        menu_pages = root.get_children().live().in_menu()
+        root.active = current_page.url == root.url if current_page else False
+        if menu_pages:
+            try:
+                for page in menu_pages:
+                    page.active = (
+                        current_page.url.startswith(
+                            page.url) if current_page else False
+                    )
+            except ValueError:
+                print("Valure error for {}".format(page))
+    except AttributeError:
+        print("Error in root: {}:{}".format(root, current_page))
+        menu_pages = []
+    # menu_pages = []
     return {
         "request": request,
         "root": root,
